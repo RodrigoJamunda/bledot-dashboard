e93a7d35b89d542c1526c6bab716cc8c1e472d5c
RodrigoJamunda <rodrigojamelo@gmail.com>
2025-10-04 14:50:39 -0300

    Removed testing files

--- streamlit-app/main_page.py
+++ streamlit-app/main_page.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
+
 def config_page():
     """Configures page layout"""
 
+
 def run_page():
     """Runs page script"""
-
-    st.switch_page("pages/company_metrics_page.py")
 
 
 if __name__ == "__main__":
